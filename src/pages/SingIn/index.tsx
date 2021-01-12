5b72fb105ec26bcb0454f68f5dd40359fb09cb67
Jean Oliveira Miranda <mirandajean2009@gmail.com>
2021-01-12 01:52:20 -0300

    Configurando a navegaçao

---
+++ src/pages/SingIn/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+const SingIn: React.FC = () => {
+  return <Container />;
+};
+
+export default SingIn;
---
+++ src/routes/index.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import SingIn from '../pages/SingIn';
+import SingUp from '../pages/SingUp';
+
+const Auth = createStackNavigator();
+
+const AuthRoutes: React.FC = () => (
+  <Auth.Navigator
+    screenOptions={{
+      headerShown: false,
+      cardStyle: {
+        backgroundColor: '#312e38',
+      },
+    }}
+  >
+    <Auth.Screen name="SingIn" component={SingIn} />
+    <Auth.Screen name="SingUp" component={SingUp} />
+  </Auth.Navigator>
+);
+
+export default AuthRoutes;
