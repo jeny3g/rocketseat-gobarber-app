c14d51df01edf04623d580cbe8c3678f866719be
Jean Oliveira Miranda <mirandajean2009@gmail.com>
2021-01-14 23:54:27 -0300

    Rotas provadas

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -20,6 +20,7 @@ interface SigInCredentials {
 
 interface AuthContextData {
   user: object;
+  loading: boolean;
   singIn(credentials: SigInCredentials): Promise<void>;
   signOut(): void;
 }
@@ -28,6 +29,7 @@ const AuthContext = createContext<AuthContextData>({} as AuthContextData);
 
 const AuthProvider: React.FC = ({ children }) => {
   const [data, setData] = useState<AuthState>({} as AuthState);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     async function loadStoragedData(): Promise<void> {
@@ -38,6 +40,8 @@ const AuthProvider: React.FC = ({ children }) => {
       if (token[1] && user[1]) {
         setData({ token: token[1], user: JSON.parse(user[1]) });
       }
+
+      setLoading(false);
     }
 
     loadStoragedData();
@@ -66,7 +70,7 @@ const AuthProvider: React.FC = ({ children }) => {
   }, []);
 
   return (
-    <AuthContext.Provider value={{ user: data.user, singIn, signOut }}>
+    <AuthContext.Provider value={{ user: data.user, loading, singIn, signOut }}>
       {children}
     </AuthContext.Provider>
   );
